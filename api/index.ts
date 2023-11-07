44e37b8720a504c84f6e58f92b17ed1f216b4422
NivN7 <145776478+NivN7@users.noreply.github.com>
2023-11-07 19:28:08 +0530

    Connect to database

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,25 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+import connectDB from "./db/connect.ts";
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000...");
-});
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL).then(() => {
+      console.log("Connected to MongoDB!");
+    });
+
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
