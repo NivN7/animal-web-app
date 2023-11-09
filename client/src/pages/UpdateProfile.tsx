d296405a1ea0944a1f62ee140b567de61ae1770f
NivN7 <145776478+NivN7@users.noreply.github.com>
2023-11-09 23:12:43 +0530

    Create updateprofile ui

--- client/src/pages/UpdateProfile.tsx
+++ client/src/pages/UpdateProfile.tsx
@@ -1,5 +1,56 @@
+import { useSelector } from "react-redux";
+
+import Button from "../components/Button";
+import Input from "../components/Input";
+import { UserStateInterface } from "../redux/user/userSlice";
+
 const UpdateProfile = () => {
-  return <div className="text-textColor">UpdateProfile</div>;
+  const { currentUser } = useSelector(
+    (state: { user: UserStateInterface }) => state.user
+  );
+
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <form className="flex flex-col gap-4">
+        <img
+          src={currentUser.avatar}
+          alt="profile"
+          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+        />
+
+        <Input
+          type="text"
+          placeholder="username"
+          id="username"
+          onChange={() => {}}
+        />
+
+        <Input
+          type="email"
+          placeholder="email"
+          id="email"
+          onChange={() => {}}
+        />
+
+        <Input
+          type="password"
+          placeholder="password"
+          id="password"
+          onChange={() => {}}
+        />
+
+        <Button
+          onClick={() => {}}
+          primaryColor={true}
+          px="px-3"
+          py="py-3"
+          rounded="rounded-lg"
+        >
+          UPDATE
+        </Button>
+      </form>
+    </div>
+  );
 };
 
 export default UpdateProfile;
